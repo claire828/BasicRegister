babf1c934342408f6d5a9e42bedf8f343a43c040
Claire <38717793+claire828@users.noreply.github.com>
2018-11-20 15:19:25 +0800

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,69 @@
+﻿using Battle.View;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Main
+{
+    public enum DepositUIStyle
+    {
+        Common = 1,
+        Friend = 2,
+        TopUp = 3
+    }
+
+    private BasicRegister<DepositUIStyle, IPanel> UIManager;
+
+
+    public Main()
+    {
+        RegisterPanel();
+    }
+
+
+    private void RegisterPanel()
+    {
+        UIManager = new BasicRegister<DepositUIStyle, IPanel>();
+        UIManager.RegisterType(DepositUIStyle.Friend, typeof(FriendPanel));
+        UIManager.RegisterType(DepositUIStyle.TopUp, typeof(TopUpPanel));
+        UIManager.RegisterType(DepositUIStyle.Common, typeof(CommonPanel));
+    }
+
+
+  
+    public void GenerateImp(DepositUIStyle style)
+    {
+        var imp = UIManager.Resolve(style); 
+    }
+
+    public void GenerateImpWithConstructor(DepositUIStyle style, object[] arrs)
+    {
+        var imp = UIManager.Resolve(style, arrs);
+    }
+
+}
+
+
+
+
+
+class FriendPanel : IPanel
+{
+    public void Execute() { Debug.Log("generating FriendPanel success "); }
+
+}
+
+class TopUpPanel : IPanel
+{
+    public void Execute() { Debug.Log("generating TopUpPanel success "); }
+
+}
+
+class CommonPanel : IPanel
+{
+    public void Execute() { Debug.Log("generating CommonPanel success "); }
+
+}
+
+
